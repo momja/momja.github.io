58768a21d08e676a335dea60e8f55706ec89296a
Max Omdal <mjomdal@gmail.com>
2020-05-21 23:07:51 -0500

    upgraded projects

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Navbar Toggle
 window.addEventListener('load', function () {
     // Get all "navbar-burger" elements
-    console.log("FUUCK");
     var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
