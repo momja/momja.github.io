2cd8673bd6fc5c36fea1d8b9867a0ee08eecf1c7
Max Omdal <mjomdal@gmail.com>
2022-12-10 11:10:22 -0800

    add favicon (smiley)

--- runBuild.sh
+++ runBuild.sh
@@ -4,6 +4,7 @@ mkdir -p static/images
 cp -r images/*.pdf static/images/
 cp -r images/*.mp4 static/images/
 cp -r images/*.mov static/images/
+cp -r favicon/144x144.png static/favicon.png
 mogrify -resize 720x720\> -format jpg -path static/images/ images/*.png
 mogrify -resize 720x720\> -path static/images/ images/*.jpg
 mogrify -resize 720x720\> -path static/images/ images/*.gif
