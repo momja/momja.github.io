48367f6d60589289ac70c0fb9308ac989fbcf095
Max Omdal <mjomdal@gmail.com>
2025-01-02 09:22:58 -0800

    Add image resizer

--- runBuild.sh
+++ runBuild.sh
@@ -5,9 +5,6 @@ cp -r images/*.pdf static/images/
 cp -r images/*.mp4 static/images/
 cp -r images/*.mov static/images/
 cp -r favicon/144x144.png static/favicon.png
-mogrify -resize 720x720\> -format jpg -path static/images/ images/*.png
-mogrify -resize 720x720\> -path static/images/ images/*.jpg
-mogrify -resize 720x720\> -path static/images/ images/*.gif
 cp -r rss.xml static/rss.xml
 
 echo "Website is published locally at file://$(dirname $(realpath "$0"))/static/index.html"
